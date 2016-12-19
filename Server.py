c3a28ec8fff98fd2d3b8ef308e2f4dfc87d95855
Amit Marcus <amitmarcus@gmail.com>
2016-12-19 23:26:01 +0200

    Added routing and "hot city" page

--- Server.py
+++ Server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, send_from_directory, jsonify
+from flask import Flask, send_from_directory, jsonify, render_template
 import MySQLdb
 
 app = Flask(__name__)
@@ -9,35 +9,49 @@ app = Flask(__name__)
 # Serve static files like js, img, css etc.
 @app.route('/<folder>/<fileName>')
 def serveStatic(folder, fileName):
-    if folder in ('js', 'img', 'css', 'lib', 'fonts'):
+    if folder in ('js', 'img', 'css', 'lib', 'fonts', 'views'):
     	print type(folder), type(fileName)
         return send_from_directory(folder, fileName)
     
-    return "ERROR"
+    return "404"
 
-@app.route("/mosaic/")
+@app.route("/api/mosaic/")
 def mosaic():
     l = []
-    l.append({"category": "ART_EVENT", "name": "אומנות בכיכר", "description":"אומנות בכיכר הוא אירוע מיוחד במינו שקורה פעם בשנה בו עושים מלא מלא מלא אומנות בכיכר"})
-    l.append({"category": "BOOK_EVENT", "name": "ספרים רבותיי", "description":
+    l.append({"event_category": "ART_EVENT", "event_name": "אומנות בכיכר1111", "event_description":"אומנות בכיכר הוא אירוע מיוחד במינו שקורה פעם בשנה בו עושים מלא מלא מלא אומנות בכיכר"})
+    l.append({"event_category": "BOOK_EVENT", "event_name": "ספרים רבותיי", "event_description":
               "מה עוד נאמר על עם הספר..."})
-    l.append({"category": "FOOD_EVENT", "name": "פסטיבל האוכל", "description":
+    l.append({"event_category": "FOOD_EVENT", "event_name": "פסטיבל האוכל", "event_description":
               "מלא מלא אוכל בלה בלה בלה אוכל בלה אוכל."})    
-    l.append({"category": "ART_EVENT", "name": "אומנות בכיכר", "description":"אומנות בכיכר הוא אירוע מיוחד במינו שקורה פעם בשנה בו עושים מלא מלא מלא אומנות בכיכר"})
-    l.append({"category": "BOOK_EVENT", "name": "ספרים רבותיי", "description":
+    l.append({"event_category": "ART_EVENT", "event_name": "אומנות בכיכר", "event_description":"אומנות בכיכר הוא אירוע מיוחד במינו שקורה פעם בשנה בו עושים מלא מלא מלא אומנות בכיכר"})
+    l.append({"event_category": "BOOK_EVENT", "event_name": "ספרים רבותיי", "event_description":
               "מה עוד נאמר על עם הספר..."})
-    l.append({"category": "FOOD_EVENT", "name": "פסטיבל האוכל", "description":
+    l.append({"event_category": "FOOD_EVENT", "event_name": "פסטיבל האוכל", "event_description":
               "מלא מלא אוכל בלה בלה בלה אוכל בלה אוכל."})    
-    l.append({"category": "ART_EVENT", "name": "אומנות בכיכר", "description":"אומנות בכיכר הוא אירוע מיוחד במינו שקורה פעם בשנה בו עושים מלא מלא מלא אומנות בכיכר"})
-    l.append({"category": "BOOK_EVENT", "name": "ספרים רבותיי", "description":
+    l.append({"event_category": "ART_EVENT", "event_name": "אומנות בכיכר", "event_description":"אומנות בכיכר הוא אירוע מיוחד במינו שקורה פעם בשנה בו עושים מלא מלא מלא אומנות בכיכר"})
+    l.append({"event_category": "BOOK_EVENT", "event_name": "ספרים רבותיי", "event_description":
               "מה עוד נאמר על עם הספר..."})
-    l.append({"category": "FOOD_EVENT", "name": "פסטיבל האוכל", "description":
+    l.append({"event_category": "FOOD_EVENT", "event_name": "פסטיבל האוכל", "event_description":
               "מלא מלא אוכל בלה בלה בלה אוכל בלה אוכל."})    
     
     return jsonify(l)
 
+@app.route("/api/city/")
+def city():
+    events = []
+    events.append({"city_name": "Tel-Aviv", "event_id": 1, "event_category": "ART_EVENT", "event_name": "אומנות בכיכר1111", "event_description":"אומנות בכיכר הוא אירוע מיוחד במינו שקורה פעם בשנה בו עושים מלא מלא מלא אומנות בכיכר"})
+    
+    d = {"city_name": "Tel-Aviv", "events": events}
+    
+    return jsonify(d)
+
+@app.route("/<path>")
+@app.route("/<path>/")
+def index(path):
+	return send_from_directory("", "index.html")
+
 @app.route("/")
-def index():
+def index2():
 	return send_from_directory("", "index.html")
 
 if __name__ == "__main__":
--- js/mouse.js
+++ js/mouse.js
@@ -1,10 +1,33 @@
-angular.module('mouse', ['mouse.controllers']);
+var mouse = angular.module('mouse', ['mouse.controllers', 'ngRoute']);
+
+mouse.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/mosaic.html',
+            controller: 'mosaic'
+        })
+        .when('/city/', {
+            templateUrl: 'views/city.html',
+            controller: 'city'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+    $locationProvider.html5Mode(true);
+});
 
 angular.module('mouse.controllers', [])
-    .controller('events', function ($scope, $http) {
-        $http.get('http://localhost:7000/mosaic/')
+    .controller('mosaic', function ($scope, $http) {
+        $http.get('http://localhost:7000/api/mosaic/')
             .then(function (response) {
                 console.log(response);
                 $scope.mosaic = response.data;
+            });
+    })
+    .controller('city', function ($scope, $http) {
+        $http.get('http://localhost:7000/api/city/')
+            .then(function (response) {
+                console.log(response);
+                $scope.data = response.data;
             });
     });
